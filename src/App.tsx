87ad6d27fde76065655ed55e2270791dc3584b76
danishm4557 <91935259+danishm4557@users.noreply.github.com>
2024-12-30 00:14:27 -0600

    Added Vercel SpeedInsight package to app for tracking site performance and other metrics on the Vercel dashboard online.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import Main from "./sections/main";
 
 function App() {
   return (
     <>
       <Main />
+      <SpeedInsights />
     </>
   );
 }
