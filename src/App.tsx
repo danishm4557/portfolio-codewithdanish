7e7bbe83ba0564c35b1fd7e35cba915f5a913c6d
danishm4557 <91935259+danishm4557@users.noreply.github.com>
2024-12-30 19:05:31 -0600

    Created a new desktop navigation. Will create a separate mobile navigation next. Created new website logo on canva and added to the application at the top in navigation. Updated styling a bit more to fit overlay and navigation content correctly.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import Main from "./sections/main";
+import Navigation from "./sections/navigation/desktop";
 
 function App() {
   return (
     <>
+      <Navigation />
       <Main />
       <SpeedInsights />
     </>
